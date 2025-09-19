3f20fc755e3959d2bbf29347fa37095f9efae819
TycOnee <mksubeesh05@gmail.com>
2025-09-19 05:14:32 +0000

    cmdline fix

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -12,6 +12,7 @@ static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
 	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
 		return 0;
 	}
 #endif
